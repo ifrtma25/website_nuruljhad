3bce800f41c95c04c6de33d0e0e202e59666b90e
ifrtma25 <muhshafarifratama@gmail.com>
2024-03-08 23:52:58 +0800

    8 Maret 2024

--- resources/views/includes/landpage/navbar.blade.php
+++ resources/views/includes/landpage/navbar.blade.php
@@ -2,8 +2,9 @@
     <div class="container">
         <div class="row">
             <div class="col-md-8 text-md-left pl-0">
-                <a href="{{ route('landpage.dashboard') }}" class="p-3"><img
+                <a href="{{ asset('assets/landpage/images/Pesantren.png') }}" target="blankspace" class="p-3"><img
                         src="{{ asset('assets/landpage/images/Pesantren.png') }}" width="50px" alt=""></a>
+                <a href="{{ route('landpage.dashboard') }}" class="p-3">Beranda</a>
                 <a href="{{ route('index.nurul_jihad') }}" class="p-3">Masjid Nurul Jihad</a>
                 <a href="{{ route('index.raudhatul_athfal') }}" class="p-3">TK/TPA Raudhatul Athfal</a>
                 <a href="{{ route('index.rumah_tahfidz') }}" class="p-3">Rumah Tahfidz</a>
